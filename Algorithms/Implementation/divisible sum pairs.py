f84faa2ea7842c177486a4f68af9f89e91a2267d
Varun Ramakrishnan <varunramakrishnan@Varuns-MacBook-Pro.local>
2018-01-27 14:14:46 +0530

    Solved Divisible Sum Pair

--- Algorithms/Implementation/divisible sum pairs.py
+++ Algorithms/Implementation/divisible sum pairs.py
@@ -1,13 +1,25 @@
+'''input
+6 3
+1 3 2 6 1 12	
+'''
+
 #!/bin/python
 
 import sys
 
 def divisibleSumPairs(n, k, ar):
 	count = 0
+	index = 0
 	for i in ar:
-		for j in r
+		for j in ar[index+1:]:
+			if (i +j) % k == 0:
+				count += 1
+
+		index += 1 
+
+	print count
 
 n, k = raw_input().strip().split(' ')
 n, k = [int(n), int(k)]
 ar = map(int, raw_input().strip().split(' '))
-divisibleSumPairs()
+divisibleSumPairs(n, k, ar)
